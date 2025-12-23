afab8aca0c6074156f159c2feb29838b5fa6e612
kyasaramvennelareddy <vennelareddy8096@gmail.com>
2025-12-23 15:41:28 +0530

    Added initial files and updated changes

---
+++ 05-Arithematic_operators.js
@@ -0,0 +1,29 @@
+let num1=4
+let num2=6
+//let result= num1+num2
+// let result= num1-num2
+// let result= num1*num2
+// let result=num1/num2/
+let result=num1%num2
+console.log(result);
+
+let b1=true
+let b2=true
+
+let r1=b1+b2
+console.log(r1);
+
+//we are getting 2 as a result it will convert true as 1 and false as 0 
+//do operations according to it 
+// ++ means adding 1 
+// value+=2 adds 2 to the value 
+// -- means subtracting  1 
+// value-=2 subtracts 2 to the value 
+//num++ means post increment
+//++num means pre increment
+//num-- means post decrement
+//--num means pre decrement 
+
+// let final=Math.pow(4,3)//4^3
+let final = 4**3
+console.log(final)
---
+++ 06-relational_oper.js
@@ -0,0 +1,9 @@
+//the output will be in boolean format 
+console.log(5>6);
+let data=7<6
+console.log(data);
+// we use >,<,=,<=,>=,==,!= ,===(strict equal to (it will check value as well as type )),!==(strict not equal to(it will check value as well as type ))
+//we can compare number and nunber ,string to string ,string to number (it will convert and compare)
+
+
+
